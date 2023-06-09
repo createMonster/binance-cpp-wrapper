c48c3852e7009b48f76d69548e19b949982edf3d
LarryCao <wenloncao2@gmail.com>
2023-06-09 12:36:25 +0100

    Add binance utils

--- src/binace_utils.cpp
+++ src/binace_utils.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include "binacpp_utils.h"
 
 using std::toupper;
+
+void split_string(string &s, char delim, vector<string> &result) {
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        result.push_back(item);
+    }
+}
+
+int replace_string_once(string &str, const char *from, const char *to, int offset) {
+    size_t start_pos = str.find(from, offset);
+    if (start_pos == std::string::npos) {
+        return 0;
+    }
+    str.replace(start_pos, strlen(from), to);
+    return start_pos + strlen(to);
+}
+
 
 void string_toupper(string &src) {
     for (int i=0; i < src.size(); i ++){
--- src/binacpp_logger.cpp
+++ src/binacpp_logger.cpp
@@ -5,3 +5,18 @@ string BinaCPP_logger::debug_log_file = "/tmp/binawatch.log";
 int    BinaCPP_logger::debug_log_file_enable = 0;
 FILE  *BinaCPP_logger::log_fp = NULL;
 
+void
+BinaCPP_logger::write_log(const char *fmt, ...) {
+    if (debug_level == 0) {
+        return;
+    }
+    if (debug_log_file_enable == 1) {
+        open_logfp_if_not_opened();
+    }
+
+    va_list arg;
+    
+    char new_fmt[1024];
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+}
